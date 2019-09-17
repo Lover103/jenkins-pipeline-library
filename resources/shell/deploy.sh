68ce4a35cf04849fbb9874adf6af0b379b0fe6c4
shiquan <454936716@qq.com>
2019-09-17 10:24:29 +0800

    fix bug

--- resources/shell/deploy.sh
+++ resources/shell/deploy.sh
@@ -8,10 +8,10 @@ module=`echo ${module%_*}`
 module=`echo ${module##*/}`
 
 # echo "容器列表：" + docker ps
-docker ps | grep ${module}:${branch}-latest | awk '{print \$1}' | xargs docker kill || true
+docker ps | grep ${module}:${branch}-latest | awk '{print $1}' | xargs docker kill || true
 
 # echo "镜像列表：" + docker images
-docker images | grep ${module}:${branch}-latest | awk '{print \$1":"\$2}' | xargs docker rmi -f || true
+docker images | grep ${module}:${branch}-latest | awk '{print $1":"$2}' | xargs docker rmi -f || true
 
 # echo "拉取镜像："
 docker pull ${module}:${branch}-latest
